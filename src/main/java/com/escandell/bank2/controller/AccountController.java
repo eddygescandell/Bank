34626991444113f9da6c797f55f1789ef0a12c5e
Eddy <eddygescandell@gmail.com>
2022-07-29 19:07:32 -0400

    Cuentas creadas y enlazadas con clientes

--- src/main/java/com/escandell/bank2/controller/AccountController.java
+++ src/main/java/com/escandell/bank2/controller/AccountController.java
@@ -1,15 +1,86 @@
 package com.escandell.bank2.controller;
 
-import com.escandell.bank2.persistence.repository.AccountRepository;
+import com.escandell.bank2.persistence.entity.Account;
+import com.escandell.bank2.persistence.entity.Coustumer;
+import com.escandell.bank2.service.AccountService;
+import com.escandell.bank2.service.CoustumerService;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class AccountController {
-     private final AccountRepository repository;
+     private final AccountService accountService;
+     private final CoustumerService coustumerService;
 
-    public AccountController(AccountRepository repository) {
-        this.repository = repository;
+    public AccountController(AccountService accountService, CoustumerService coustumerService) {
+        this.accountService = accountService;
+        this.coustumerService = coustumerService;
     }
+
+
+    @GetMapping(path = "/account")
+    public ModelAndView Account(){
+        return new ModelAndView("account/accountform")
+                .addObject("account",new Account())
+                .addObject("coustumerlis", coustumerService.ListAllCoustumer());
+    }
+    @PostMapping(path = "/account")
+    public String AddAccount(@Validated Account account, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("account", account);
+            model.addAttribute("coustumerlis", coustumerService.ListAllCoustumer());
+            return "account/accountform";
+        }
+        accountService.CreatedAccount(account);
+        redirectAttributes.addFlashAttribute( "msgSucces", "The Account has been created successfully");
+        return "redirect:/accountlist";
+    }
+    @GetMapping(path = "/accountlist")
+    public ModelAndView AccountList(){
+        return new ModelAndView("account/accountlist")
+                .addObject("accountlist", accountService.ListAllAccount());
+    }
+    @GetMapping(path = "/{id}/editaccount")
+    public ModelAndView GetAccountById(@PathVariable Long id){
+        Account account= accountService.GetAccountById(id);
+        Coustumer coustumer = coustumerService.GetCoustumerById(account.getCoustumer().getId());
+        return new ModelAndView("account/accountform").addObject("account", account).addObject("coustumerlis",coustumer);
+    }
+
+    @PostMapping(path = "/{id}/editaccount")
+    public String EditCoustumerById(@PathVariable Long id,@Validated Account account,BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model){
+        Account accountDB = accountService.GetAccountById(id);
+        if(bindingResult.hasErrors()){
+            model.addAttribute("account", account);
+            model.addAttribute("coustumerlis", coustumerService.ListAllCoustumer());
+            return "account/accountform";
+        }
+        accountDB.setBalance(account.getBalance());
+
+
+        accountService.CreatedAccount(accountDB);
+        redirectAttributes.addFlashAttribute( "msgSucces", "The Account has been update successfully");
+        return "redirect:/accountlist ";
+    }
+
+    @GetMapping(path = "/{id}/deletaccount")
+    public ModelAndView DeleteAccount (@PathVariable(name = "id") Long id, ModelAndView view){
+        accountService.DeleteAccountById(id);
+        view.setViewName("account/accountlist");
+        view.addObject("accountlist", accountService.ListAllAccount());
+        return view;
+
+    }
+
+
 
 
 }
--- src/main/java/com/escandell/bank2/persistence/entity/Account.java
+++ src/main/java/com/escandell/bank2/persistence/entity/Account.java
@@ -2,21 +2,14 @@ package com.escandell.bank2.persistence.entity;
 
 
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDateTime;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PastOrPresent;
 
-import org.hibernate.annotations.ManyToAny;
 
 import lombok.Data;
 
@@ -27,17 +20,24 @@ public class Account implements Serializable {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Digits(integer = 8, fraction = 0)
 	private Long id;
-	
+
+
 	@Min(value = 50, message = "El saldo debe ser mayor o igual a 50")
 	private Long balance;
-	
-	@PastOrPresent
-	private Date creationDate;
-	
-//	/*many to one union con coustumer*/
-//	@ManyToOne(fetch =FetchType.LAZY, optional = false)
-//	@JoinColumn(name = "coustumer_id", nullable = false)
-//	private Coustumer coustumer;
 
+
+	@PastOrPresent
+	private LocalDateTime creationDate;
+	
+	/*many to one union con coustumer*/
+	@ManyToOne
+	@JoinColumn(name = "coustumer_id")
+	private Coustumer coustumer;
+
+	//Asignar fecha de creacion a las cuentas automaticamente
+	@PrePersist
+	public void AddCreationDate(){
+		creationDate = LocalDateTime.now();
+	}
 
 }
